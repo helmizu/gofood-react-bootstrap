aa2877eca4137dd3c11dd652ef5c7b3ac4bb3b08
Helmi Zulfikar <helmi.zulfikar20@gmail.com>
2022-12-16 21:42:29 +0700

    Setup command to deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://helmizu.github.io/gofood-react-bootstrap/'
 })
